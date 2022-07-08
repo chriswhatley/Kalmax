939c27e73b1f4b47aca68834b69d75cea7777726
Chris Whatley <chriswhatley81@hotmail.com>
2022-07-08 14:39:47 +0100

    Scroll offset backup height correction

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
-// calculate the height of the fixed nav and create a css height property to offset the scroll button target
+require('./bootstrap');
+
+// calculate the height of the fixed nav and create a css height variable to offset the scroll button target so it's not covered by the fixed nav
 const navigationHeight = document.querySelector('.primary-navigation').offsetHeight;
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + "px");
